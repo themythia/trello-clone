374e511232ed395ae870bbd6760ea2751c752493
Emir AL <36728456+themythia@users.noreply.github.com>
2021-08-23 17:11:43 +0300

    implemented ui of cardmodal

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import styled from 'styled-components';
 import { Draggable } from 'react-beautiful-dnd';
+import { BsPencil } from 'react-icons/bs';
+import CardModal from './CardModal';
 
 const Card = ({ task, index }) => {
   const [hover, setHover] = useState(false);
+  const [showModal, setShowModal] = useState('false');
+  const [position, setPosition] = useState(null);
+  const element = useRef('');
+
+  useEffect(() => {
+    const rect = element.current.getBoundingClientRect();
+    setPosition({
+      x: rect.x,
+      y: rect.y,
+      top: rect.top,
+      bottom: rect.bottom,
+      left: rect.left,
+      right: rect.right,
+      width: rect.width,
+    });
+    console.log(rect);
+  }, []);
+  // const rect = element.current.getBoundingClientRect();
+  // if (rect && rect.current) {
+  //   console.log(rect);
+  // }
+
   // example of snapshot object
 
   // draggable
@@ -19,21 +43,39 @@ const Card = ({ task, index }) => {
   // }
   console.log('hover:', hover);
   return (
-    <Draggable draggableId={task.id} index={index}>
-      {(provided, snapshot) => (
-        <div
-          onMouseEnter={() => setHover(true)}
-          onMouseLeave={() => setHover(false)}
-          className='card-container'
-          {...provided.draggableProps}
-          {...provided.dragHandleProps}
-          ref={provided.innerRef}
-          isDragging={snapshot.isDragging}
-        >
-          {task.content}
-        </div>
+    <React.Fragment>
+      <Draggable draggableId={task.id} index={index}>
+        {(provided, snapshot) => (
+          <div
+            onMouseEnter={() => setHover(true)}
+            onMouseLeave={() => setHover(false)}
+            className='card-container'
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+            ref={provided.innerRef}
+            isDragging={snapshot.isDragging}
+          >
+            <div ref={element}>
+              {task.content}
+              {hover === true && (
+                <BsPencil
+                  onClick={() => setShowModal(true)}
+                  className='card-container-icon'
+                />
+              )}
+            </div>
+          </div>
+        )}
+      </Draggable>
+      {showModal === true && (
+        <CardModal
+          show={showModal}
+          onClose={() => setShowModal(false)}
+          position={position}
+          task={task}
+        />
       )}
-    </Draggable>
+    </React.Fragment>
   );
 };
 export default Card;
---
+++ src/components/CardModal.js
@@ -0,0 +1,72 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  BsCardText,
+  BsFillTagFill,
+  BsCardImage,
+  BsTrashFill,
+} from 'react-icons/bs';
+import { FaCopy } from 'react-icons/fa';
+
+const CardModal = ({ show, onClose, position, task }) => {
+  const [height, setHeight] = useState(929);
+  const [width, setWidth] = useState(1920);
+  const [input, setInput] = useState(task.content);
+  const textarea = useRef(null);
+
+  const dispatch = useDispatch();
+  console.log('task', task);
+
+  useEffect(() => {
+    textarea.current.select();
+    setHeight(window.innerHeight);
+    setWidth(window.innerWidth);
+  }, []);
+
+  if (show === false) {
+    return null;
+  }
+  console.log('position', position);
+
+  return (
+    <div className='card-modal-div' onClick={onClose}>
+      <div
+        style={{
+          top: position.top - height / 2 + 80,
+          left: position.left - width / 2 + position.width / 2 + 60,
+        }}
+        className='card-modal-content'
+        onClick={(e) => e.stopPropagation()}
+      >
+        <div className='left'>
+          <textarea
+            ref={textarea}
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            autoFocus
+            spellCheck={false}
+          />
+          <button className='text-save-btn'>Save</button>
+        </div>
+        <div className='right'>
+          <button className='side-btn'>
+            <BsCardText size={16} className='side-btn-icon' /> Open card
+          </button>
+          <button className='side-btn'>
+            <BsFillTagFill size={16} className='side-btn-icon' /> Edit labels
+          </button>
+          <button className='side-btn'>
+            <BsCardImage size={16} className='side-btn-icon' /> Change cover
+          </button>
+          <button className='side-btn'>
+            <FaCopy size={16} className='side-btn-icon' /> Copy
+          </button>
+          <button className='side-btn'>
+            <BsTrashFill size={16} className='side-btn-icon' /> Delete
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default CardModal;
